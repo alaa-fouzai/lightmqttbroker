2fc3b5385b0ce40e0eb9e30c8cbeedf000ae3b57
alaa <fouzai.alaa@gmail.com>
2023-09-30 19:03:31 +0100

    fix user authentication

--- index.js
+++ index.js
@@ -6,86 +6,86 @@ const ws = require('websocket-stream')
 const axios = require('axios');
 
 server.listen(process.env.MQTT_Port, function () {
-    console.log('Aedes MQTT server started and listening on port ', process.env.MQTT_Port)
+  console.log('Aedes MQTT server started and listening on port ', process.env.MQTT_Port)
 })
 
 ws.createServer({ server: httpServer }, aedes.handle)
 httpServer.listen(process.env.wsPort, function () {
-    console.log('websocket server listening on port ', process.env.wsPort)
+  console.log('websocket server listening on port ', process.env.wsPort)
 })
 
 // authentication
 aedes.authenticate = async (client, login, password, callback) => {
-    password = Buffer.from(password, 'base64').toString();
-    console.log( "UUID :",client.id," login",login,"password :",password)
-    
-    //Auth = password.split('!:!')
-    //let url = process.env.AuthLightSwitchURLlocal+":"+process.env.AuthLightSwitchPORT+process.env.AuthLightSwitchPATH;
-    let url = process.env.AuthLightSwitchURL+process.env.AuthLightSwitchPATH;
-    let uuid= "";
-    if (client.id.includes("app-")) {
-      console.log("app");
-      uuid = client.id.substring(4);
-    } else if (client.id.includes("device-")){
-      console.log("device");
-      uuid = client.id.substring(7);
-    } else {
-      console.log("uuid error");
-      return ;
+  password = Buffer.from(password, 'base64').toString();
+  console.log("UUID :", client.id, " login", login, "password :", password)
+
+  let url = process.env.AuthLightSwitchURL;
+  let uuid = "";
+  if (client.id.includes("app-")) {
+    console.log("app");
+    uuid = client.id.substring(4);
+    url = url + process.env.userAuthPATH;
+  } else if (client.id.includes("device-")) {
+    console.log("device");
+    uuid = client.id.substring(7);
+    url = url + process.env.AuthLightSwitchPATH;
+  } else {
+    console.log("uuid error");
+    return;
+  }
+  response = await axios({
+    method: "post",
+    url: url,
+    data: {
+      client: uuid,
+      login: login,
+      password: password
     }
-    response = await axios({
-      method: "post",
-      url: url,
-      data: {
-        client :uuid,
-        login: login,
-        password: password
-      }
-    }).then(function (response) {
-      if (response.data.status==='ok' && response.data.message === 'success') {
-        return callback(null, true);
-      }else {
-        const error = new Error('Authentication Failed!! Please enter valid credentialsssss.');
-        return callback(error, false)
-      }
-    }).catch(function (error) {
-      if (error.response) {
-        // The request was made and the server responded with a status code
-        // that falls out of the range of 2xx
-        console.log("error.response.data");
-        console.log(error.response.data);
-        console.log(error.response.status);
-        console.log(error.response.headers);
-      } else if (error.request) {
-        // The request was made but no response was received
-        // `error.request` is an instance of XMLHttpRequest in the browser 
-        // and an instance of http.ClientRequest in node.js
-        console.log("error.request");
-        console.log(error.request);
-      } else {
-        // Something happened in setting up the request that triggered an Error
-        console.log('Error', "error.message");
-        console.log('Error', error.message);
-      }
-     
-    });
+  }).then(function (response) {
+    if (response.data.status === 'ok' && response.data.message === 'success') {
+      return callback(null, true);
+    } else {
+      const error = new Error('Authentication Failed!! Please enter valid credentialsssss.');
+      return callback(error, false)
+    }
+  }).catch(function (error) {
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.log("error.response.data");
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+    } else if (error.request) {
+      // The request was made but no response was received
+      // `error.request` is an instance of XMLHttpRequest in the browser 
+      // and an instance of http.ClientRequest in node.js
+      console.log("error.request");
+      console.log(error.request);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.log('Error', "error.message");
+      console.log('Error', error.message);
+    }
+
+  });
 }
 
 // authorising client topic to publish a message
 aedes.authorizePublish = (client, packet, callback) => {
-    if (packet.topic === 'abc') {
-        return callback(new Error('wrong topic'));
-    }
-    if (packet.topic === 'charcha') {
-        packet.payload = Buffer.from('overwrite packet payload')
-    }
-    callback(null)
+  if (packet.topic === 'abc') {
+    return callback(new Error('wrong topic'));
+  }
+  if (packet.topic === 'charcha') {
+    packet.payload = Buffer.from('overwrite packet payload')
+  }
+  callback(null)
 }
 
 // emitted when a client connects to the broker
 aedes.on('client', function (client) {
-    console.log(`CLIENT_CONNECTED : MQTT Client ${(client ? client.id : client)} connected to aedes broker ${aedes.id}`)
-  })
+  console.log(`CLIENT_CONNECTED : MQTT Client ${(client ? client.id : client)} connected to aedes broker ${aedes.id}`)
+})
 // emitted when a client disconnects from the broker
 aedes.on('clientDisconnect', function (client) {
   console.log(`CLIENT_DISCONNECTED : MQTT Client ${(client ? client.id : client)} disconnected from the aedes broker ${aedes.id}`)
@@ -99,17 +99,19 @@ aedes.on('unsubscribe', function (subscriptions, client) {
   console.log(`TOPIC_UNSUBSCRIBED : MQTT Client ${(client ? client.id : client)} unsubscribed to topic: ${subscriptions.join(',')} from aedes broker ${aedes.id}`)
 })
 // emitted when a client publishes a message packet on the topic
-aedes.on('publish', function (packet, client) {if (client) {
-  console.log(`MESSAGE_PUBLISHED : MQTT Client ${(client ? client.id : 'AEDES BROKER_' + aedes.id)} has published message "${packet.payload}" on ${packet.topic} to aedes broker ${aedes.id}`)}
+aedes.on('publish', function (packet, client) {
+  if (client) {
+    console.log(`MESSAGE_PUBLISHED : MQTT Client ${(client ? client.id : 'AEDES BROKER_' + aedes.id)} has published message "${packet.payload}" on ${packet.topic} to aedes broker ${aedes.id}`)
+  }
 })
 
 
 
-  async function AutonticateSwitch(url,data) {
-    try {
-      const response = await axios.post(url, data);
-      console.log(response);
-    } catch (error) {
-      console.error(error);
-    }
+async function Autonticate(url, data) {
+  try {
+    const response = await axios.post(url, data);
+    console.log(response);
+  } catch (error) {
+    console.error(error);
   }
+}
